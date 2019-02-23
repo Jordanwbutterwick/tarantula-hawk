8295a71c14b648deca3afc8b04c2a4a8d5b70610
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-02-23 13:59:37 -0700

    creating modules

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Cormorant+Garamond|Lato"
+      rel="stylesheet"
+    />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="./css/index.css" />
+    <title>The Tarantula Hawk</title>
+  </head>
+  <body>
+    <?php
+@include_once "header.php";    
+?>
+    <main>
+      <div class="hero"></div>
+      <div class="content">
+        <h1>The Tarantula Hawk</h1>
+      </div>
+    </main>
+    <?php
+@include_once "./php/footer.php"
+?>
+    <script src="./scripts/scripts.js"></script>
+  </body>
+</html>
